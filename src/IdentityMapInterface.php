be78ebdea4221a953933275ed04b79f2c24c753a
Andrey <alestro@bk.ru>
2020-07-03 22:58:19 +0200

    Update IdentityMapInterface.php

--- src/IdentityMapInterface.php
+++ src/IdentityMapInterface.php
@@ -1,67 +1,66 @@
 <?php
 
 
-namespace Lobster\Identity;
+namespace Bermuda\Identity;
 
 
 /**
- * interface IdentityMapInterface
- * @package Lobster\Identity
+ * interface IdentityInterface
+ * @package Bermuda\Identity
  */
-interface IdentityMapInterface extends \IteratorAggregate {
-
+interface IdentityInterface extends \IteratorAggregate 
+{
     /**
      * @param object $object
-     * @throws InvalidObjectException
-     * @throws DuplicateObjectException
-     * @return IdentityMapInterface
+     * @throws IdentityException
+     * @return IdentityInterface
      */
-    public function add(object $object) : self ;
+    public function add(object $object): self ;
     
     /**
      * @param string $cls
      * @param object $object
-     * @throws DuplicateObjectException
-     * @return IdentityMapInterface
+     * @throws IdentityException
+     * @return IdentityInterface
      */
-    public function set(string $cls, object $object) : self ;
+    public function set(string $cls, object $object): self ;
 
     /**
      * @param string $cls
      * @param string $id
      * @return bool
      */
-    public function has(string $cls, string $id) : bool ;
+    public function has(string $cls, string $id): bool ;
     
     /**
      * @param string $cls
      * @param string $id
      * @return object|null
      */
-    public function get(string $cls, string $id) :? object ;
+    public function get(string $cls, string $id):? object ;
 
     /**
      * @param string $cls
      * @param string $id
      * @return IdentityMap
      */
-    public function remove(string $cls, string $id) : self ;
+    public function remove(string $cls, string $id): self ;
 
     /**
      * @param object $obj
      * @return IdentityMap
      */
-    public function removeObject(object $obj) : self ;
+    public function removeObject(object $obj): self ;
 
     /**
      * @param object $object
      * @return string|null
      */
-    public function idOf(object $object) :? string ;
+    public function idOf(object $object):? string ;
     
     /**
      * @param object $object
      * @return bool
      */
-    public function hasObject(object $object) : bool ;
+    public function hasObject(object $object): bool ;
 }
